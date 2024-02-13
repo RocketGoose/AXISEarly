0ac96d5e34ae656a5015fa0ae306200fbbca8315
RocketGoose <40300361+RocketGoose@users.noreply.github.com>
2024-02-13 16:56:44 +0000

    Update alumina_refinery.py

--- src/industries/alumina_refinery.py
+++ src/industries/alumina_refinery.py
@@ -11,6 +11,7 @@ industry = IndustrySecondary(
     name="string(STR_IND_ALUMINA_REFINERY)",
     nearby_station_name="string(STR_STATION_SMELTER)",
     fund_cost_multiplier="200",
+    intro_year=1890,
     graphics_change_dates=[],
 )
 
